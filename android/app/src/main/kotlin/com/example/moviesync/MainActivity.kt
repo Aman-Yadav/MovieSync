ed5a4b7fdf6e11afa962490c68f1a919a3515e5d
Aman Yadav <yadavaman1081@gmail.com>
2021-08-17 23:06:07 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/moviesync/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moviesync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/movie_list_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MovieListScreen extends StatefulWidget {
+  @override
+  _MovieListScreenState createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () => print('Navigate to add movie screen'),
+      ),
+    );
+  }
+}
